e9ba7dbdb3cf9f9b0582b95b5d4d44817f32d041
caketower <dlaqudtn1107@naver.com>
2025-08-27 12:21:37 +0900

    [8강] JAP에서 특정 글에 관한 질문글을 보는 방법

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -1,10 +1,15 @@
 package com.mysite.sbb;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,5 +22,12 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+
+    @OneToMany
+    //하나의 컬럼에는 하나의 값만 들어가야한다. answers는 리스트로 여러값이니 컬럼으로 생성되지않는다.
+    //그래도 DB에는 answers가 따로 테이블로 생긴다.
+    //JPA는 어떻게든 DB를 사용할수 있게 만들어주기때문
+    //OneToMany같은 실제 DB관계가 아니라 객체사이의 개념적인 관계로 생각하자
+    private List<Answer> answers;   //1대N관계라서 answer가 여러개 일수있음
 
 }
--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -7,7 +7,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbbApplication.class, args);
+		//Answer a = new Answer();
+
+        //Question question = a.getQuestion();
+        //question.getContent();
+
+        //5번 질문글을 조회
+        //Question q5 = questionRepository.findById(5).get();
+
+        //5번 글에 대한 답글 조회
+        //answerRepository.findAll()  -> X
+         //answerRepository.findById(5) -> X -> 이건 id가 5번인 답글을 가져오는거지 5번 글에 대한 답글을 가져오는게 아님
+         //answerRepository.findQuestion(q5) => O
+        //List<Answer> answers = answerRepository.findQuestionId(5) => O
+        //위 방법들은 DB적이고 객체적인 방법들도 있다.
+
+        //객체적인 방법은
+        //List<Answer> answers =  q5.getAnswers();
+
+
+        //질문을 조회하는 방법
+
+        SpringApplication.run(SbbApplication.class, args);
 	}
 
 }
